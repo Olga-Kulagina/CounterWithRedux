58367716b6e4b6b6523d24ef812a7767b8578936
Olga-Kulagina <olga.kulagina17@yandex.ru>
2020-11-04 17:28:46 +0300

    Updates

---
+++ precache-manifest.f83892274a2979e009b62416496a96f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265b06be41381f9f1a2088973062540a",
+    "url": "/CounterWithRedux/index.html"
+  },
+  {
+    "revision": "a544999f2a9cf28c879c",
+    "url": "/CounterWithRedux/static/css/main.62f41976.chunk.css"
+  },
+  {
+    "revision": "e0fdeccda268ff550eef",
+    "url": "/CounterWithRedux/static/js/2.a35214e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CounterWithRedux/static/js/2.a35214e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a544999f2a9cf28c879c",
+    "url": "/CounterWithRedux/static/js/main.6d939596.chunk.js"
+  },
+  {
+    "revision": "5462236376525bb53b11",
+    "url": "/CounterWithRedux/static/js/runtime-main.9bd2866f.js"
+  }
+]);
